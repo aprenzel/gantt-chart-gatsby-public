ab1fd2da562050b7e1181b227578dfdbf1a43fe0
aprenzel <36474485+aprenzel@users.noreply.github.com>
2021-10-11 22:34:46 +0200

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import {GanttChart} from "../GanttChart"
+import { useState, useEffect } from 'react';
 import '../styles/index.css';
 
 // markup
 const IndexPage = (data) => {
-
-  console.log(data.data);
 
   let j = data.data.jobs.edges.map(edge => {
     
@@ -17,13 +16,14 @@ const IndexPage = (data) => {
     e.setHours(0);
 
     return {
+      airtable_id: edge.node.recordId,
       id:edge.node.data.id,
       start: s,
       end: e,
-      resource: parseInt(edge.node.data.id__from_resource_[0])
+      resource: edge.node.data.id__from_resource_[0],
+      resource_airtable_id: edge.node.data.resource[0]
     };
   });
-
 
   let r = data.data.resources.edges.map(edge => {
 
@@ -33,12 +33,43 @@ const IndexPage = (data) => {
     }
   });
 
-  if(r && j){
+  const [resources, setResources] = useState(r);
+  const [jobs, setJobs] = useState(j);
+
+  useEffect(() => {
+    const interval = setInterval(() => { 
+      
+      let jobsLoaded = (j) => { setJobs(j) };
+      let resourcesLoaded = (r) => { setResources(r) };
+
+      loadDataFromAirtable(jobsLoaded, resourcesLoaded);
+    
+    }, 60000);  
+
+    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+  }, []);
+
+  let updateJob = (id, newJob) => {
+
+    let new_jobs = jobs.slice();
+
+    let job = new_jobs.find(j => j.id == id );
+
+    job.resource = newJob.resource;
+    job.start = newJob.start;
+    job.end = newJob.end;
+  
+    setJobs(new_jobs);
+
+    updateJobToAirtable(job);
+  }
+
+  if(resources && jobs){
     return (
       <main>
         <title>Gantt Chart</title>
         <h1>Welcome to my Gatsby Gantt Chart</h1> 
-        <GanttChart jobs={j} resources={r}/> 
+        <GanttChart jobs={jobs} resources={resources} onUpdateJob={updateJob}/> 
       </main>
     )
   }else{
@@ -52,9 +83,93 @@ const IndexPage = (data) => {
   }
 }
 
+
+function updateJobToAirtable(job){
+
+    let data = {
+      records: [
+      {
+        id: job.airtable_id,
+        fields: {
+          id: job.id,
+          start: formatDate(job.start),
+          end: formatDate(job.end),
+          resource: [
+            job.resource_airtable_id
+          ]
+        }
+      }
+    ]};
+
+  fetch("XXX", {
+    method: "PATCH", 
+    headers: {"Authorization": "ZZZ", "Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  });
+}
+
+function loadDataFromAirtable(onJobsLoaded, onResourcesLoaded){
+
+  let j,r;
+
+  fetch('XXX',
+        {headers: {"Authorization": "ZZZ"}})
+  .then(response => response.json())
+  .then(data => {
+
+    j = data.records.map(record => {
+
+      let s = new Date(record.fields.start);
+      s.setHours(0);
+  
+      let e = new Date(record.fields.end);
+      e.setHours(0);
+
+       return {
+        airtable_id: record.id,
+        id: record.fields.id,
+        start: s,
+        end: e,
+        resource: record.fields['id (from resource)'][0],
+        resource_airtable_id: record.fields.resource[0]
+       };
+    });
+
+    onJobsLoaded(j);
+  });
+
+
+  fetch('YYY',{headers: {"Authorization": "ZZZ"}})
+  .then(response => response.json())
+  .then(data => {
+
+    r = data.records.map(record => {
+
+       return {
+        id: record.fields.id,
+        name: record.fields.name
+       };
+    });
+
+    onResourcesLoaded(r);
+  });
+}
+
+function formatDate(d){
+
+  return d.getFullYear()+"-"+zeroPad(d.getMonth()+1)+"-"+zeroPad(d.getDate());
+
+}
+
+function zeroPad(n){
+
+  return n<10 ? "0"+n : n;
+
+}
+
 export const query = graphql`
       query{
-        jobs: allAirtable(filter: {table: {eq: "Jobs"}}) {
+        jobs: allAirtable(filter: {table: {eq: "Jobs"}, data: {}}) {
           edges {
             node {
               data {
@@ -62,15 +177,16 @@ export const query = graphql`
                 start
                 end
                 id__from_resource_
+                resource
               }
+              recordId
             }
           }
         }
-
         resources: allAirtable(
           filter: {table: {eq: "Resources"}}
           sort: {fields: [data___name], order: ASC}
-          ) {
+        ) {
           edges {
             node {
               data {
@@ -82,7 +198,5 @@ export const query = graphql`
         }
       }
   `
-  
-
 export default IndexPage
 
